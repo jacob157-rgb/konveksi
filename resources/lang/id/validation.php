f61f1ce0dac87ecf030759b74013b79b9beefcc7
Rizkiputraramadhan <rizkikuliit@gmail.com>
2024-07-14 12:53:37 +0700

    lang

---
+++ resources/lang/id/validation.php
@@ -0,0 +1,141 @@
+<?php
+
+return [
+    'accepted'             => ':attribute harus diterima.',
+    'active_url'           => ':attribute bukan URL yang valid.',
+    'after'                => ':attribute harus tanggal setelah :date.',
+    'after_or_equal'       => ':attribute harus tanggal setelah atau sama dengan :date.',
+    'alpha'                => ':attribute hanya boleh mengandung huruf.',
+    'alpha_dash'           => ':attribute hanya boleh mengandung huruf, angka, dan strip.',
+    'alpha_num'            => ':attribute hanya boleh mengandung huruf dan angka.',
+    'array'                => ':attribute harus berupa sebuah array.',
+    'before'               => ':attribute harus tanggal sebelum :date.',
+    'before_or_equal'      => ':attribute harus tanggal sebelum atau sama dengan :date.',
+    'between'              => [
+        'numeric' => ':attribute harus antara :min dan :max.',
+        'file'    => ':attribute harus antara :min dan :max kilobytes.',
+        'string'  => ':attribute harus antara :min dan :max karakter.',
+        'array'   => ':attribute harus memiliki antara :min dan :max item.',
+    ],
+    'boolean'              => ':attribute harus berupa true atau false.',
+    'confirmed'            => 'Konfirmasi :attribute tidak cocok.',
+    'date'                 => ':attribute bukan tanggal yang valid.',
+    'date_equals'          => ':attribute harus berupa tanggal yang sama dengan :date.',
+    'date_format'          => ':attribute tidak cocok dengan format :format.',
+    'different'            => ':attribute dan :other harus berbeda.',
+    'digits'               => ':attribute harus berupa :digits digit.',
+    'digits_between'       => ':attribute harus antara :min dan :max digit.',
+    'dimensions'           => ':attribute memiliki dimensi gambar yang tidak valid.',
+    'distinct'             => ':attribute memiliki nilai yang duplikat.',
+    'email'                => ':attribute harus berupa alamat surel yang valid.',
+    'ends_with'            => ':attribute harus diakhiri salah satu dari: :values.',
+    'exists'               => ':attribute yang dipilih tidak valid.',
+    'file'                 => ':attribute harus berupa sebuah berkas.',
+    'filled'               => ':attribute harus memiliki nilai.',
+    'gt'                   => [
+        'numeric' => ':attribute harus lebih besar dari :value.',
+        'file'    => ':attribute harus lebih besar dari :value kilobytes.',
+        'string'  => ':attribute harus lebih besar dari :value karakter.',
+        'array'   => ':attribute harus memiliki lebih dari :value item.',
+    ],
+    'gte'                  => [
+        'numeric' => ':attribute harus lebih besar dari atau sama dengan :value.',
+        'file'    => ':attribute harus lebih besar dari atau sama dengan :value kilobytes.',
+        'string'  => ':attribute harus lebih besar dari atau sama dengan :value karakter.',
+        'array'   => ':attribute harus memiliki :value item atau lebih.',
+    ],
+    'image'                => ':attribute harus berupa gambar.',
+    'in'                   => ':attribute yang dipilih tidak valid.',
+    'in_array'             => ':attribute tidak ada di :other.',
+    'integer'              => ':attribute harus berupa bilangan bulat.',
+    'ip'                   => ':attribute harus berupa alamat IP yang valid.',
+    'ipv4'                 => ':attribute harus berupa alamat IPv4 yang valid.',
+    'ipv6'                 => ':attribute harus berupa alamat IPv6 yang valid.',
+    'json'                 => ':attribute harus berupa JSON string yang valid.',
+    'lt'                   => [
+        'numeric' => ':attribute harus kurang dari :value.',
+        'file'    => ':attribute harus kurang dari :value kilobytes.',
+        'string'  => ':attribute harus kurang dari :value karakter.',
+        'array'   => ':attribute harus memiliki kurang dari :value item.',
+    ],
+    'lte'                  => [
+        'numeric' => ':attribute harus kurang dari atau sama dengan :value.',
+        'file'    => ':attribute harus kurang dari atau sama dengan :value kilobytes.',
+        'string'  => ':attribute harus kurang dari atau sama dengan :value karakter.',
+        'array'   => ':attribute tidak boleh lebih dari :value item.',
+    ],
+    'max'                  => [
+        'numeric' => ':attribute tidak boleh lebih dari :max.',
+        'file'    => ':attribute tidak boleh lebih dari :max kilobytes.',
+        'string'  => ':attribute tidak boleh lebih dari :max karakter.',
+        'array'   => ':attribute tidak boleh lebih dari :max item.',
+    ],
+    'mimes'                => ':attribute harus berupa berkas dengan tipe: :values.',
+    'mimetypes'            => ':attribute harus berupa berkas dengan tipe: :values.',
+    'min'                  => [
+        'numeric' => ':attribute harus minimal :min.',
+        'file'    => ':attribute harus minimal :min kilobytes.',
+        'string'  => ':attribute harus minimal :min karakter.',
+        'array'   => ':attribute harus memiliki minimal :min item.',
+    ],
+    'not_in'               => ':attribute yang dipilih tidak valid.',
+    'not_regex'            => 'Format :attribute tidak valid.',
+    'numeric'              => ':attribute harus berupa angka.',
+    'password'             => 'Kata sandi salah.',
+    'present'              => ':attribute harus ada.',
+    'regex'                => 'Format :attribute tidak valid.',
+    'required'             => ':attribute wajib diisi.',
+    'required_if'          => ':attribute wajib diisi bila :other adalah :value.',
+    'required_unless'      => ':attribute wajib diisi kecuali :other memiliki nilai :values.',
+    'required_with'        => ':attribute wajib diisi bila terdapat :values.',
+    'required_with_all'    => ':attribute wajib diisi bila terdapat :values.',
+    'required_without'     => ':attribute wajib diisi bila tidak terdapat :values.',
+    'required_without_all' => ':attribute wajib diisi bila tidak terdapat salah satu dari :values.',
+    'same'                 => ':attribute dan :other harus sama.',
+    'size'                 => [
+        'numeric' => ':attribute harus berukuran :size.',
+        'file'    => ':attribute harus berukuran :size kilobytes.',
+        'string'  => ':attribute harus berukuran :size karakter.',
+        'array'   => ':attribute harus mengandung :size item.',
+    ],
+    'starts_with'          => ':attribute harus diawali salah satu dari: :values.',
+    'string'               => ':attribute harus berupa string.',
+    'timezone'             => ':attribute harus berupa zona waktu yang valid.',
+    'unique'               => ':attribute sudah ada.',
+    'uploaded'             => ':attribute gagal diunggah.',
+    'url'                  => 'Format :attribute tidak valid.',
+    'uuid'                 => ':attribute harus berupa UUID yang valid.',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify custom validation messages for attributes using the
+    | convention "attribute.rule" to name the lines. This makes it quick to
+    | specify a specific custom language line for a given attribute rule.
+    |
+    */
+
+    'custom' => [
+        'attribute-name' => [
+            'rule-name' => 'custom-message',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap attribute place-holders
+    | with something more reader friendly such as "E-Mail Address" instead
+    | of "email". This simply helps us make messages a little cleaner.
+    |
+    */
+
+    'attributes' => [
+        'kalender_akademik_id' => 'kalender akademik',
+        'jam_kerja_id' => 'jam kerja',
+    ],
+];
